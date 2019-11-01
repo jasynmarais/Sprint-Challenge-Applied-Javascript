492a1049c240d549f2c308776acbfd534657c9a8
Jasyn Marais <jasynmarais@gmail.com>
2019-11-01 10:31:28 +0200

    Add all elements to DOM.

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,16 +10,31 @@
 // And add it to the DOM in the .headerContainer component
 
 function Header() {
+    // Add elements to header component
     const header = document.createElement('div');
     const date = document.createElement('span');
     const title = document.createElement('h1');
     const temperature = document.createElement('span');
 
+    // Add classes to header elements
     header.classList.add('header');
     date.classList.add('date');
     temperature.classList.add('temp');
 
+    // Add textContent
     date.textContent = 'MARCH 28, 2019';
     title.textContent = 'Lambda Times';
     temperature.textContent = '98°';
+
+    // Append all elements to DOM
+    document.querySelector('.header-container').appendChild(header);
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temperature);
+
+    // Return component
+    return header;
 }
+
+    // Call header function
+    Header();
